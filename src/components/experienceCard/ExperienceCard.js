496e03f66e7ac740fe9412bf78f94518a2ee5ed2
tamirshina <tamirshina@gmail.com>
2021-09-05 16:31:33 +0300

    Revert to 56e05fced

--- src/components/experienceCard/ExperienceCard.js
+++ src/components/experienceCard/ExperienceCard.js
@@ -1,38 +1,38 @@
-import React, {createRef} from "react";
+import React, { useState, createRef } from "react";
 import "./ExperienceCard.scss";
-//import ColorThief from "colorthief";//
+import ColorThief from "colorthief";
 
-export default function ExperienceCard({cardInfo, isDark}) {
-  //const [colorArrays, setColorArrays] = useState([]);
+export default function ExperienceCard({ cardInfo, isDark }) {
+  const [colorArrays, setColorArrays] = useState([]);
   const imgRef = createRef();
 
-  // function getColorArrays() {
-  //   const colorThief = new ColorThief();
-  //   setColorArrays(colorThief.getColor(imgRef.current));
-  // }
+  function getColorArrays() {
+    const colorThief = new ColorThief();
+    setColorArrays(colorThief.getColor(imgRef.current));
+  }
 
-  // function rgb(values) {
-  //   return typeof values === "undefined"
-  //     ? null
-  //     : "rgb(" + values.join(", ") + ")";
-  // }
+  function rgb(values) {
+    return typeof values === "undefined"
+      ? null
+      : "rgb(" + values.join(", ") + ")";
+  }
 
-  const GetDescBullets = ({descBullets, isDark}) => {
+  const GetDescBullets = ({ descBullets, isDark }) => {
     return descBullets
       ? descBullets.map((item, i) => (
-          <li
-            key={i}
-            className={isDark ? "subTitle dark-mode-text" : "subTitle"}
-          >
-            {item}
-          </li>
-        ))
+        <li
+          key={i}
+          className={isDark ? "subTitle dark-mode-text" : "subTitle"}
+        >
+          {item}
+        </li>
+      ))
       : null;
   };
 
   return (
     <div className={isDark ? "experience-card-dark" : "experience-card"}>
-      <div style={{background: cardInfo.color}} className="experience-banner">
+      <div style={{ background: cardInfo.color }} className="experience-banner">
         <div className="experience-blurred_div"></div>
         <div className="experience-div-company">
           <h5 className="experience-text-company">{cardInfo.company}</h5>
@@ -44,7 +44,7 @@ export default function ExperienceCard({cardInfo, isDark}) {
           className="experience-roundedimg"
           src={cardInfo.companylogo}
           alt={cardInfo.company}
-          //onLoad={() => getColorArrays()}
+          onLoad={() => getColorArrays()}
         />
       </div>
       <div className="experience-text-details">
--- src/containers/profile/Profile.js
+++ src/containers/profile/Profile.js
@@ -1,12 +1,12 @@
-import React, {useState, useEffect, Suspense} from "react";
-import {openSource} from "../../portfolio";
+import React, { useState, useEffect, lazy, Suspense } from "react";
+import { openSource } from "../../portfolio";
 import Contact from "../contact/Contact";
 import Loading from "../loading/Loading";
 
 const renderLoader = () => <Loading />;
-// const GithubProfileCard = lazy(() =>
-//   import("../../components/githubProfileCard/GithubProfileCard")
-// );
+const GithubProfileCard = lazy(() =>
+  import("../../components/githubProfileCard/GithubProfileCard")
+);
 export default function Profile() {
   const [prof, setrepo] = useState([]);
   function setProfileFunction(array) {
