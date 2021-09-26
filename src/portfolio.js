e88edf06a88b935e713453c171da3f7df6435434
tamirshina <tamirshina@gmail.com>
2021-09-26 15:42:32 +0300

    text

--- src/portfolio.js
+++ src/portfolio.js
@@ -48,7 +48,7 @@ const skillsSection = {
       "⚡ Build highly efficient API's and do all the server side work an app needs"
     ),
     emoji(
-      "⚡ Also desktop app's I have experince with c# and JAVA devloping for windows"
+      "⚡ Desktop app's I also have experince with c# and JAVA devloping for windows"
     )
   ],
 
@@ -287,7 +287,7 @@ const achievementSection = {
       subtitle: "Completed Certifcation from SMIT for PWA Web App Development",
       image: require("./assets/images/pwaLogo.webp"),
       footerLink: [
-        {name: "Certification", url: ""},
+        { name: "Certification", url: "" },
         {
           name: "Final Project",
           url: "https://pakistan-olx-1.firebaseapp.com/"
